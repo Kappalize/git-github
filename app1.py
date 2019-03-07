f279734f07d56a34d0b390595eeeb418a76e9565
Kappalize <samnatangwe@live.com>
2019-03-07 05:25:50 +0200

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,14 @@
+
+def main():
+    namibia()
+    angola()
+
+def namibia():
+    bird = 5000
+    print('Namibia has' bird, 'bird.')
+
+def angola():
+    bird = 3000
+    print('Angola has' bird, 'bird')
+
+main()
